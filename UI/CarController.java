da359c61097ff09de776f8a6effc2c518350dfa8
AnonyAnt <alexlysholm99@gmail.com>
2019-02-15 18:18:14 +0100

    CarController should not be responsible for creating the timer nor the TimeListener

--- UI/CarController.java
+++ UI/CarController.java
@@ -2,7 +2,6 @@ package UI;
 
 import Cars.Car;
 
-import javax.swing.*;
 import java.util.ArrayList;
 
 /*
@@ -16,21 +15,13 @@ import java.util.ArrayList;
 class CarController {
     // member fields:
 
-    // The delay (ms) corresponds to 20 updates a sec (hz)
-    private final int delay = 50;
-    // The timer is started with an listener (see below) that executes the statements
-    // each step between delays.
-    private Timer timer;
 
-    // The frame that represents this instance View of the MVC pattern
-   // private CarView frame;
     // A list of cars, modify if needed
     private ArrayList<Car> cars;
     private int gasAmount;
 
-    CarController(String name) {
+    CarController() {
         cars = new ArrayList<>();
-        timer = new Timer(delay, new TimerListener(cars,new CarView(name, this)));
         gasAmount = 0;
 
     }
@@ -43,10 +34,10 @@ class CarController {
         cars.add(car);
     }
 
-
-    void start() {
-        timer.start();
+    public ArrayList<Car> getCars() {
+        return cars;
     }
+
 
     // Calls the gas method for each car once
     void gas() {
--- UI/RunUI.java
+++ UI/RunUI.java
@@ -2,16 +2,25 @@ package UI;
 
 import Cars.Volvo240;
 
+import javax.swing.*;
+
 /*
  * This class should only be responsible for starting up the UI
  */
 public class RunUI {
 
+    // The delay (ms) corresponds to 20 updates a sec (hz)
+    private static final int delay = 50;
+
     public static void main(String[] args) {
-        // Instance of this class
-        CarController cc = new CarController("test");
+        CarController cc = new CarController();
         cc.addCar(new Volvo240());
-        cc.start();
+
+        CarView frame = new CarView("test", cc);
+        TimerListener tl = new TimerListener(cc.getCars(), frame);
+
+        Timer timer = new Timer(delay, tl);
+        timer.start();
 
     }
 }
