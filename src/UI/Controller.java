74d82cafce8f2a0d3e32490dc5386190e7426107
Ausut <robert.finne@gmail.com>
2019-02-27 16:13:22 +0100

    carview->controller->carcontroller

--- src/UI/Controller.java
+++ src/UI/Controller.java
@@ -4,8 +4,45 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Controller implements ActionListener {
+    CarController carC;
+
+    public void setCarController(CarController carC) {
+        this.carC = carC;
+    }
+
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
+        String command = actionEvent.getActionCommand();
+        switch(command) {
+            case "Gas":
+                carC.gas();
+                break;
+            case "Brake":
+                carC.brake();
+                break;
+            case "Saab Turbo On":
+                carC.turboOn();
+                break;
+            case "Saab Turbo Off":
+                carC.turboOff();
+                break;
+            case "Scania Lift Bed":
+                carC.liftBed();
+                break;
+            case "Lower Lift Bed":
+                carC.lowerBed();
+                break;
+            case "Start all cars":
+                carC.start();
+                break;
+            case "Stop all cars":
+                carC.stop();
+                break;
+        }
 
+    }
+    public void setGasBrakeAmount(int amount) {
+        carC.setGasAmount(amount);
+        carC.setBrakeAmount(amount);
     }
 }
